dfe7960e3bb60224762aefdeafed57d187fd045c
VonEquinox <VonEquinoxf@gmail.com>
2026-04-20 22:02:58 +0800

    feat(mdx): 支持KaTeX数学公式

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,18 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [
+    react(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()]
